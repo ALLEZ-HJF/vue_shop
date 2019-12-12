85521fd7d9c53ac5672830e29d726f04dc256e5f
ALLEZ-HJF <254534216@qq.com>
2019-12-12 15:53:43 +0800

    完成了分类功能开发

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import './assets/css/global.css'
 import './assets/fonts/iconfont.css'
 // 引入axios
 import axios from 'axios'
+// 引入treeTable
+import TreeTable from 'vue-table-with-tree-grid'
 Vue.prototype.$http = axios
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 axios.interceptors.request.use(config => {
@@ -14,7 +16,7 @@ axios.interceptors.request.use(config => {
   return config
 })
 Vue.config.productionTip = false
-
+Vue.component('tree-table', TreeTable)
 new Vue({
   router,
   render: h => h(App)
